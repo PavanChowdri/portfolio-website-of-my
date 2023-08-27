18fb21bc1f41c6c6586f2a3fcce859db436e1562
pavan <90958934+Dimplechoudhary-m@users.noreply.github.com>
2023-08-27 18:16:19 +0530

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 /*------------------ typing animation --------------*/
 var typed=new Typed(".typing",{
-    strings:["","Web Developer","Frelancer"],
+    strings:["","Web Developer","Freelancer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
